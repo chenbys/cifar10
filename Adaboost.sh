9eb1d47085346abd13a0d638d0f778d86c6368c5
Junjie Chen <chen.bys@outlook.com>
2019-01-16 23:09:35 +0800

    'm'

--- Adaboost.sh
+++ Adaboost.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-python min_for_Adaboost.py --name A1 --pca_dim 320 --n_estimators 100 --max_depth 4
-python min_for_Adaboost.py --name A2 --pca_dim 320 --n_estimators 200 --max_depth 4
-python min_for_Adaboost.py --name A3 --pca_dim 320 --n_estimators 300 --max_depth 4
-python min_for_Adaboost.py --name A4 --pca_dim 320 --n_estimators 400 --max_depth 4
-python min_for_Adaboost.py --name A5 --pca_dim 320 --n_estimators 500 --max_depth 4
-python min_for_Adaboost.py --name A6 --pca_dim 320 --n_estimators 600 --max_depth 4
+python main_for_Adaboost.py --name A1 --pca_dim 320 --n_estimators 100 --max_depth 4
+python main_for_Adaboost.py --name A2 --pca_dim 320 --n_estimators 200 --max_depth 4
+python main_for_Adaboost.py --name A3 --pca_dim 320 --n_estimators 300 --max_depth 4
+python main_for_Adaboost.py --name A4 --pca_dim 320 --n_estimators 400 --max_depth 4
+python main_for_Adaboost.py --name A5 --pca_dim 320 --n_estimators 500 --max_depth 4
+python main_for_Adaboost.py --name A6 --pca_dim 320 --n_estimators 600 --max_depth 4
